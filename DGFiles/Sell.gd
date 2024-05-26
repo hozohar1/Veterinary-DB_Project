02b0f6705ae25b48cb3e724274f9291285eb4fed
Hodie <116975069+hozohar1@users.noreply.github.com>
2024-05-26 22:25:46 +0300

    Add files via upload

---
+++ DGFiles/Sell.gd
@@ -0,0 +1,32 @@
+
+[General]
+Version=1
+
+[Preferences]
+Username=
+Password=2379
+Database=
+DateFormat=
+CommitCount=0
+CommitDelay=0
+InitScript=
+
+[Table]
+Owner=EWEKSLER
+Name=SELL
+Count=400
+
+[Record]
+Name=ACCID
+Type=NUMBER
+Size=
+Data=Random(0, 9999999)
+Master=
+
+[Record]
+Name=SID
+Type=NUMBER
+Size=
+Data=Random(0, 9999999)
+Master=
+
