16010f3813da73e14bc2f0dbd0d37eb189c159e6
eF <116486421+Efrat-W@users.noreply.github.com>
2024-06-17 15:16:50 +0300

    Update ParamQueries.sql

--- stage 2/ParamQueries.sql
+++ stage 2/ParamQueries.sql
@@ -11,16 +11,16 @@ FROM
     JOIN Vet ON Appointment.sID = Vet.sID
     JOIN Staff ON Vet.sID = Staff.sID
 WHERE
-    Staff.sName = &name; -- vet name parameter
-    
+    Staff.sName = &<name="vet name" list="SELECT staff.sName FROM Vet JOIN Staff ON vet.sId = staff.sId" hint="add single quotation marks around the vet name. Example: 'Elad'">;
+   
 -- Receipt of purchase
 SELECT AccName, AccPrice
 FROM Accessories
-WHERE AccId IN (&list)
+WHERE AccId IN (&barcode)
 UNION ALL
 SELECT 'Total', SUM(AccPrice) AS TotalPrice
 FROM Accessories
-WHERE AccId IN (&list);
+WHERE AccId IN (&barcode);
 
 -- List vets and their appointments between 2 param dates
 SELECT Staff.sName, Appointment.AppDate, Pet.petName, PetOwner.ownerName AS petOwnerName
@@ -29,10 +29,10 @@ FROM Pet
   JOIN Appointment ON Pet.petId = Appointment.petId
   JOIN Vet ON Appointment.sID = Vet.sID
   JOIN Staff ON Vet.sID = Staff.sID
-WHERE Appointment.AppDate BETWEEN TO_DATE('&start', 'YYYY-MM-DD') AND TO_DATE('&end', 'YYYY-MM-DD');
+WHERE Appointment.AppDate BETWEEN TO_DATE('&from', 'DD-MM-YYYY') AND TO_DATE('&to', 'DD-MM-YYYY');
 
--- List all appointments of an owner pets
-SELECT 
+-- List all appointments of an owner's pets
+SELECT
     PetOwner.ownerName,
     PetOwner.ownerPhoneNumber,
     Pet.petName,
@@ -43,5 +43,12 @@ FROM PetOwner
   JOIN Appointment ON Pet.petId = Appointment.petId
   JOIN treatmentType ON Appointment.AppID = treatmentType.AppID
   JOIN Treatment ON treatmentType.tID = Treatment.tID
-WHERE 
-    PetOwner.ownerID = &id;
+WHERE
+    PetOwner.ownerID = &<name="id" type="integer">;
+
+-- Show all details about chosen owners from the multiselect list
+SELECT * FROM PetOwner WHERE ownerID IN
+(&<name="owner_id"
+list="SELECT ownerID, ownerName FROM PetOwner ORDER BY ownerName"
+description="yes"
+multiselect="yes">);
